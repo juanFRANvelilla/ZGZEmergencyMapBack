4be0ab92337269e5c58fa0bef95d83f501505294
juanFRANvelilla <juanfran01velilla@gmail.com>
2024-08-12 18:06:06 +0200

    first commit

---
+++ src/main/java/com/example/zgzemergencymapback/ZgzEmergencyMapBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.zgzemergencymapback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZgzEmergencyMapBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZgzEmergencyMapBackApplication.class, args);
+    }
+
+}
