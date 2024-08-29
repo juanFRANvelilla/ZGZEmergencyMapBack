6ad361fa174a213611b08ab4c2e1cc79e7d63100
juanFRANvelilla <juanfran01velilla@gmail.com>
2024-08-29 22:12:33 +0200

    tarea progaramda

--- src/main/java/com/example/zgzemergencymapback/task/ScheduledTask.java
+++ src/main/java/com/example/zgzemergencymapback/task/ScheduledTask.java
@@ -1,20 +1,20 @@
-//package com.example.zgzemergencymapback.task;
-//
-//import com.example.zgzemergencymapback.service.impl.IncidentsZgzDataServiceImpl;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class ScheduledTask {
-//
-//    @Autowired
-//    private IncidentsZgzDataServiceImpl incidentsZgzDataService;
-//
-//    // Este método se ejecutará cada 15 minutos (900,000 ms)
-//    @Scheduled(fixedRate = 900000)
-//    public void executeTask() {
-//        System.out.println("Ejecutar tarea programada");
-//        incidentsZgzDataService.reloadTodayEmergency();
-//    }
-//}
+package com.example.zgzemergencymapback.task;
+
+import com.example.zgzemergencymapback.service.impl.IncidentsZgzDataServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTask {
+
+    @Autowired
+    private IncidentsZgzDataServiceImpl incidentsZgzDataService;
+
+    // Este método se ejecutará cada 15 minutos (900,000 ms)
+    @Scheduled(fixedRate = 900000)
+    public void executeTask() {
+        System.out.println("Ejecutar tarea programada");
+        incidentsZgzDataService.reloadTodayEmergency();
+    }
+}
